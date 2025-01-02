40bf0c3bf4c6c2d23b2e0e03e26e933e75f590b7
NovikoffArtem <142929498+NovikoffArtem@users.noreply.github.com>
2025-01-02 23:30:57 +0300

    2

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,28 @@
-// https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
+import Swiper from 'swiper';
+import { Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+export const setupJurySwiper = () => {
+  if(document.querySelector('.hero__wrapper')){
+    new Swiper('.hero__wrapper', {
+      loop: true,
+      slidesPerView: 1,
+      allowTouchMove: true,
+
+      modules: [Pagination],
+
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+
+      breakpoints: {
+        1440: {
+          allowTouchMove: false,
+        },
+      },
+    });
+  }
+};
+setupJurySwiper();
